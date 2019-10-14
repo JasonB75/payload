ed57bd54a022e222e6a7a11f34e820c1a9df3a8e
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2019-10-14 08:06:39 -0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,16 +9,24 @@ camera = PiCamera()
 
 
 def recordButtonPressed(time)
-    
+#TAB THESE
+camera.start_preview()
+sleep(2)
+camera.capture('/home/pi/Desktop/image.jpg')
+camera.start_recording('/home/pi/Desktop/video.h264')
+sleep(2)
+camera.stop_recording()
+camera.stop_preview()
 
 
-# GPIO 23 set up as input. It is pulled up to stop false signals  
+# GPIO 23 set up as input
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 
 try:  
     GPIO.wait_for_edge(23, GPIO.FALLING)  
-    print "\nFalling edge detected. Now your program can continue with"  
+    recordButtonPressed()
     print "whatever was waiting for a button press."  
+    
 except KeyboardInterrupt:  
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
 GPIO.cleanup()           # clean up GPIO on normal exit  
